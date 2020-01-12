215eab2a9b3c4035311190d3608f5d1850b6b0c2
Mingfei Ding <xiesansi@gmail.com>
2020-01-13 00:52:44 +0800

    intro router

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const nanoid = require('nanoid')
+const generate = require('nanoid/generate');
 const Koa = require('koa');
+const Router = require('@koa/router');
 
 const { set } = require('./db')
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-    const id = nanoid();
-    const response = await set();
+const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+
+router.get('/generate', async ctx => {
+    const id = generate(alphabet, 12);
+    const response = await set({
+        id: 1
+    });
     if (response !== false) {
         ctx.body = id;
+    } else {
+        ctx.body = 'error'
     }
-});
+})
+
+app.use(router.routes());
 
 module.exports = {
     app
